9ae049ecd1d320c196486d0dbd7201eeb218c42a
nabin <nkbhaie2017@gmail.com>
2023-01-16 09:06:16 -0700

    add more styling

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ setInterval(function(){
 
 // Read more, read less
 function myFunction() {
+    // evt.preventDefault();
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
@@ -40,5 +41,27 @@ function myFunction() {
     //   btnText.innerHTML = "Read less >>";
         btnText.innerHTML = "Read more >>";
       moreText.style.display = "inline";
+    }
+  }
+
+//   jobEducation    educationButton    
+
+// Education Read less or more
+function myEducationFunction() {
+    // evt.preventDefault();
+    var dots = document.getElementById("jobEducation");
+    var moreContent = document.getElementById("moreContent");
+    var button = document.getElementById("educationButton");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      button.innerHTML = "Read less >>";
+    //   btnText.innerHTML = "Read more >>";
+        moreContent.style.display = "none";
+    } else {
+      dots.style.display = "none";
+    //   btnText.innerHTML = "Read less >>";
+        button.innerHTML = "Read more >>";
+        moreContent.style.display = "inline";
     }
   }
